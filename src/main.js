5640af0f28c1ecdc64ea5be9eb3663eac0cb5307
wariszargardev <wariszargardev@gmail.com>
2022-11-26 15:16:12 +0500

    add boostrap

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(bootstrap).mount('#app')
